2b0ff4f3f2c42935d23271684c46880187d7edfd
Zhuo Lu <sethlu@mintkit.net>
2019-03-17 18:05:36 -0700

    Remove redundant property

--- lib/ParticleNode.h
+++ lib/ParticleNode.h
@@ -21,9 +21,6 @@ protected:
     glm::dmat3 deformElastic = glm::dmat3(1);
     glm::dmat3 deformPlastic = glm::dmat3(1);
 
-    // Used for updates
-    double tempWeight;
-
     // Memoized weights for each update
     double weight[64];
     glm::dvec3 nabla_weight[64];
