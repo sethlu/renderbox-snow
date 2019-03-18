3941f67efe4e107ca8efcc2ee6ccc16814356827
Zhuo Lu <sethlu@mintkit.net>
2019-03-17 21:51:13 -0700

    Fix script & add semi-implicit slab over wedge util script

--- scripts/sim-scene1-slab-semi-implicit-5e-3.sh
+++ scripts/sim-scene1-slab-semi-implicit-5e-3.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 TIME_STEP=5e-3
-DIRECTORY=scene1-slab-d60-explicit-$TIME_STEP
+DIRECTORY=scene1-slab-d60-semi-implicit-$TIME_STEP
 FRAMES=120
 
 rm -rf $DIRECTORY
 mkdir $DIRECTORY
 cd $DIRECTORY
 
-../snow sim-gen-slab $TIME_STEP
+../snow sim-gen-slab $TIME_STEP 1
 ../snow sim-scene1 0 $FRAMES
