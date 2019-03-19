2dae121ef1b907465b5d2ce64bab0594dba8211d
Zhuo Lu <sethlu@mintkit.net>
2019-03-19 00:59:10 -0700

    Snowman with some ground snow

--- src/demo-snowman.cpp
+++ src/demo-snowman.cpp
@@ -10,6 +10,7 @@
 #include "utils/renderer.h"
 #include "scenes/scene0.h"
 #include "snow/sphere.h"
+#include "snow/slab.h"
 
 
 static unsigned ticksPerFrame = 1;
@@ -34,14 +35,17 @@ void launchDemoSnowman(int argc, char const **argv) {
 
     solver.reset(new SnowSolver(gridSize, simulationSize * (1 / gridSize)));
 
-    auto r1 = 0.2;
-    auto r2 = 0.125;
-    auto r3 = 0.075;
+    auto r1 = 0.2 / 2;
+    auto r2 = 0.125 / 2;
+    auto r3 = 0.075 / 2;
     auto overlap = 0.05;
 
-    auto c1 = 0.1 - overlap + r1;
+    auto c1 = 0.1 + r1;
     auto c2 = c1 + r1 - overlap + r2;
     auto c3 = c2 + r2 - overlap + r3;
+
+    genSnowSlab(glm::dvec3(0.05, 0.05, 0.075), glm::dvec3(simulationSize.x - 0.05, simulationSize.y - 0.05, 0.125),
+                density, particleSize);
 
     genSnowSphere(glm::dvec3(0.5, 0.5, c1), r1, density, particleSize);
     genSnowSphere(glm::dvec3(0.5, 0.5, c2), r2, density, particleSize);
